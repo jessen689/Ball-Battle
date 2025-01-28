9955a7f2496a18ad0e78bde682d4a37a8695bcd7
jessen689 <39221403+jessen689@users.noreply.github.com>
2025-01-29 01:16:17 +0700

    Energy Feature
    Energy Point, Regen, Use

---
+++ Assets/Scripts/Energy/EnergyHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BallBattle.Energy
+{
+	public class EnergyHandler : MonoBehaviour
+	{
+		[SerializeField] private Color energyColor_;
+		[SerializeField] private Transform energyParent_;
+
+		private List<EnergyPoint> energyPoints = new List<EnergyPoint>();
+		private int currEnergy;
+		private EnergyPoint tempEnergy;
+
+		private void Start()
+		{
+			InitializeEnergy();
+		}
+
+		private void InitializeEnergy()
+		{
+			if(energyPoints.Count <= 0)
+			{
+				for(int i = energyParent_.childCount - 1; i >= 0; i--)
+				{
+					energyPoints.Add(energyParent_.GetChild(i).GetComponent<EnergyPoint>());
+					energyPoints[energyPoints.Count - 1].RemoveEnergy();
+					energyPoints[energyPoints.Count - 1].SetEnergyColor(energyColor_);
+					energyPoints[energyPoints.Count - 1].OnEnergyFilled += AddEnergyPoint;
+				}
+			}
+			else
+			{
+				RemoveEnergyPoint(energyPoints.Count);
+			}
+
+			currEnergy = 0;
+			energyPoints[0].GenerateEnergy();
+		}
+
+		private void AddEnergyPoint()
+		{
+			currEnergy++;
+			if(currEnergy < energyPoints.Count)
+			{
+				energyPoints[currEnergy].GenerateEnergy();
+			}
+		}
+
+		public void UseEnergy(int cost)
+		{
+			if (cost > currEnergy)
+				return;
+
+			RemoveEnergyPoint(2);
+			//trigger use for what (?)
+
+		}
+
+		private void RemoveEnergyPoint(int _value)
+		{
+			for(int loop = 0; loop < _value; loop++)
+			{
+				tempEnergy = energyPoints[0];
+				energyPoints.Remove(tempEnergy);
+				tempEnergy.transform.SetAsFirstSibling();
+				tempEnergy.RemoveEnergy();
+				energyPoints.Add(tempEnergy);
+
+				if (currEnergy == energyPoints.Count)
+					tempEnergy.GenerateEnergy();
+				currEnergy--;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Energy/EnergyPoint.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BallBattle.Energy
+{
+	public class EnergyPoint : MonoBehaviour
+	{
+		[SerializeField] private Image energyFillImg_;
+		[SerializeField] private float regenOpacity_;
+		[SerializeField] [Tooltip("in seconds")] private float regenTime_;
+
+		private Vector3 fillScale = Vector3.one;
+		private Color energyColor;
+		private float regenCounter;
+
+		public event Action OnEnergyFilled;
+
+		public void SetEnergyColor(Color _color)
+		{
+			energyColor = _color;
+			energyFillImg_.color = energyColor;
+		}
+
+		public void GenerateEnergy()
+		{
+			regenCounter = 0;
+			energyFillImg_.color = new Color(energyFillImg_.color.r, energyFillImg_.color.g, energyFillImg_.color.b, regenOpacity_ / 255f); //divided by 255 to get value between 0 & 1
+			StartCoroutine(GeneratingEnergy());
+		}
+
+		public void RemoveEnergy()
+		{
+			fillScale.x = 0f;
+			energyFillImg_.transform.localScale = fillScale;
+		}
+
+		private IEnumerator GeneratingEnergy()
+		{
+			while(regenCounter < regenTime_)
+			{
+				regenCounter += Time.deltaTime;
+				fillScale.x = Mathf.Clamp01(regenCounter / regenTime_);
+				energyFillImg_.transform.localScale = fillScale;
+				yield return null;
+			}
+			OnEnergyFilled?.Invoke();
+			energyFillImg_.color = energyColor;
+		}
+	}
+}
